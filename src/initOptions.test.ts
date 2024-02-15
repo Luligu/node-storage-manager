fb8b7f8bc7766d207b128361a46ac517a72f6f03
Luligu <132135057+Luligu@users.noreply.github.com>
2024-02-15 12:09:04 +0100

    1.0.2

--- src/initOptions.test.ts
+++ src/initOptions.test.ts
@@ -1,47 +1,47 @@
-import { NodeStorageManager } from '../src/nodeStorage';
+import { NodeStorageManager } from './nodeStorage.js';
 import NodePersist from 'node-persist';
 
 jest.mock('node-persist', () => ({
-	create: jest.fn().mockReturnThis(),
-	initSync: jest.fn().mockImplementation((options) => options), 
-	init: jest.fn().mockResolvedValue(undefined), // Mock init method
-	set: jest.fn().mockResolvedValue(undefined), // Mock init method
+  create: jest.fn().mockReturnThis(),
+  initSync: jest.fn().mockImplementation((options) => options),
+  init: jest.fn().mockResolvedValue(undefined), // Mock init method
+  set: jest.fn().mockResolvedValue(undefined), // Mock init method
 }));
 
 jest.mock('path', () => ({
-	...jest.requireActual('path'),
-	join: jest.fn().mockImplementation((...args) => args.join('/')),
+  ...jest.requireActual('path'),
+  join: jest.fn().mockImplementation((...args) => args.join('/')),
 }));
 
 jest.mock('fs/promises', () => ({
-	rm: jest.fn().mockResolvedValue(undefined),
+  rm: jest.fn().mockResolvedValue(undefined),
 }));
- 
+
 describe('NodeStorageManager', () => {
-	const defaultDir = process.cwd() + '/node_storage';
+  const defaultDir = process.cwd() + '/node_storage';
 
-	beforeEach(() => {
-		jest.clearAllMocks();
-	});
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-	it('should create and initialize storage with default options', async () => {
-		new NodeStorageManager();
-		expect(NodePersist.create).toHaveBeenCalledWith({
-			dir: defaultDir,
-			logging: false,
-		});
-	});
+  it('should create and initialize storage with default options', async () => {
+    new NodeStorageManager();
+    expect(NodePersist.create).toHaveBeenCalledWith({
+      dir: defaultDir,
+      logging: false,
+    });
+  });
 
-	it('should merge custom init options with defaults', async () => {
-		const customOptions = { dir: 'custom_dir', logging: true };
-		const expectedOptions = {
-			dir: 'custom_dir',
-			logging: true,
-		};
+  it('should merge custom init options with defaults', async () => {
+    const customOptions = { dir: 'custom_dir', logging: true };
+    const expectedOptions = {
+      dir: 'custom_dir',
+      logging: true,
+    };
 
-		new NodeStorageManager(customOptions);
-		expect(NodePersist.create).toHaveBeenCalledWith(expectedOptions);
-	});
+    new NodeStorageManager(customOptions);
+    expect(NodePersist.create).toHaveBeenCalledWith(expectedOptions);
+  });
 
 });
 
--- src/nodeStorage.ts
+++ src/nodeStorage.ts
@@ -18,108 +18,109 @@ export type NodeStorageKey = string;
 export type NodeStorageName = string;
 
 export class NodeStorageManager {
-	private readonly storage: LocalStorage;
-	private readonly initOptions: InitOptions;
-	private storageNames: NodeStorageName[]=[];
+  private readonly storage: LocalStorage;
+  private readonly initOptions: InitOptions;
+  private storageNames: NodeStorageName[]=[];
 
-	constructor(initOptions?: InitOptions) {
-		// Merge initOptions with default initOptions
-		this.initOptions = Object.assign({
-			dir: path.join(process.cwd(), 'node_storage'),
-			logging: false,
-		} as InitOptions, initOptions);
+  constructor(initOptions?: InitOptions) {
+    // Merge initOptions with default initOptions
+    this.initOptions = Object.assign({
+      dir: path.join(process.cwd(), 'node_storage'),
+      logging: false,
+    } as InitOptions, initOptions);
 
-		// Create and initialize a new instace of LocalStorage
-		this.storage = NodePersist.create(this.initOptions);
-		this.storage.initSync(this.initOptions);
-		console.log(`Storage manager initialized with options ${JSON.stringify(this.initOptions)}`);
-		/*
+    // Create and initialize a new instace of LocalStorage
+    this.storage = NodePersist.create(this.initOptions);
+    this.storage.initSync(this.initOptions);
+    console.log(`Storage manager initialized with options ${JSON.stringify(this.initOptions)}`);
+    /*
 		this.get<Array<NodeStorageName>>('storageNames').then(storageNames => {
 			this.storageNames = storageNames;
 		});
 		*/
-	}
+  }
 
-	async createStorage(storageName: string): Promise<NodeStorage> {
-		const initOptions: InitOptions = {};
-		Object.assign(initOptions, this.initOptions, { dir: path.join(this.initOptions.dir!, '.' + storageName) } as InitOptions);
-		const storage = NodePersist.create(initOptions);
-		await storage.init(initOptions);
-		console.log(`Created storage ${storageName} with options ${JSON.stringify(initOptions)}`);
+  async createStorage(storageName: string): Promise<NodeStorage> {
+    const initOptions: InitOptions = {};
+    Object.assign(initOptions, this.initOptions, { dir: path.join(this.initOptions.dir!, '.' + storageName) } as InitOptions);
+    const storage = NodePersist.create(initOptions);
+    await storage.init(initOptions);
+    console.log(`Created storage ${storageName} with options ${JSON.stringify(initOptions)}`);
 
-		// Update storageNames
-		this.storageNames=await this.storage.get('storageNames') ?? [];
-		//console.log('Storage list(1):', this.storageNames);
-		if(!this.storageNames.includes(storageName))
-			this.storageNames.push(storageName);
-		//console.log('Storage list(2):', this.storageNames);
-		await this.storage.set('storageNames', this.storageNames);
-		//console.log('Storage list(3):', await this.storage.get('storageNames') );
+    // Update storageNames
+    this.storageNames=await this.storage.get('storageNames') ?? [];
+    //console.log('Storage list(1):', this.storageNames);
+    if(!this.storageNames.includes(storageName)) {
+      this.storageNames.push(storageName);
+    }
+    //console.log('Storage list(2):', this.storageNames);
+    await this.storage.set('storageNames', this.storageNames);
+    //console.log('Storage list(3):', await this.storage.get('storageNames') );
 
-		return new NodeStorage(storage, initOptions);
-	}
+    return new NodeStorage(storage, initOptions);
+  }
 
-	async removeStorage(storageName: string): Promise<boolean> {
-		const dir = path.join(this.initOptions.dir!, '.' + storageName);
-		try {
-			await rm(dir, { recursive: true });
-			console.log('Storage removed');
+  async removeStorage(storageName: string): Promise<boolean> {
+    const dir = path.join(this.initOptions.dir!, '.' + storageName);
+    try {
+      await rm(dir, { recursive: true });
+      console.log('Storage removed');
 
-			// Update storageNames
-			this.storageNames=await this.storage.get('storageNames') ?? [];
-			const index = this.storageNames.indexOf(storageName);
-			if (index > -1) {
-				this.storageNames.splice(index, 1); 
-			}
-			await this.storage.set('storageNames', this.storageNames);
-			//console.log('Storage list:', await this.storage.get('storageNames') );
+      // Update storageNames
+      this.storageNames=await this.storage.get('storageNames') ?? [];
+      const index = this.storageNames.indexOf(storageName);
+      if (index > -1) {
+        this.storageNames.splice(index, 1);
+      }
+      await this.storage.set('storageNames', this.storageNames);
+      //console.log('Storage list:', await this.storage.get('storageNames') );
 
-			return true;
-		} catch (err) {
-			console.error('Error removing storage:', err);
-			return false;
-		}
-	}
+      return true;
+    } catch (err) {
+      console.error('Error removing storage:', err);
+      return false;
+    }
+  }
 
-	async getStorageNames(): Promise<NodeStorageName[]> {
-		this.storageNames=await this.storage.get('storageNames') ?? [];
-		return this.storageNames;
-	}
+  async getStorageNames(): Promise<NodeStorageName[]> {
+    this.storageNames=await this.storage.get('storageNames') ?? [];
+    return this.storageNames;
+  }
 
-	async logStorage() {
-		console.log(`This NodeStorageManager has ${await this.storage.length()} storages: ${JSON.stringify(await this.storage.keys())}`);
-	}
+  async logStorage() {
+    console.log(`This NodeStorageManager has ${await this.storage.length()} storages: ${JSON.stringify(await this.storage.keys())}`);
+  }
 }
 
 export class NodeStorage {
-	private readonly storage: LocalStorage;
-	private readonly initOptions: InitOptions;
+  private readonly storage: LocalStorage;
+  private readonly initOptions: InitOptions;
 
-	constructor(storage: LocalStorage, initOptions: InitOptions) {
-		this.storage = storage;
-		this.initOptions = initOptions;
-	}
+  constructor(storage: LocalStorage, initOptions: InitOptions) {
+    this.storage = storage;
+    this.initOptions = initOptions;
+  }
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	async set<T = any>(key: NodeStorageKey, value: T): Promise<NodePersist.WriteFileResult> {
-		return await this.storage.setItem(key, value);
-	}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async set<T = any>(key: NodeStorageKey, value: T): Promise<NodePersist.WriteFileResult> {
+    return await this.storage.setItem(key, value);
+  }
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	async get<T = any>(key: NodeStorageKey, defaultValue?: T): Promise<T> {
-		const value = await this.storage.getItem(key);
-		return value!==undefined ? value: defaultValue;
-	}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async get<T = any>(key: NodeStorageKey, defaultValue?: T): Promise<T> {
+    const value = await this.storage.getItem(key);
+    return value!==undefined ? value: defaultValue;
+  }
 
-	async remove(key: NodeStorageKey): Promise<NodePersist.DeleteFileResult> {
-		return await this.storage.removeItem(key);
-	}
+  async remove(key: NodeStorageKey): Promise<NodePersist.DeleteFileResult> {
+    return await this.storage.removeItem(key);
+  }
 
-	async clear(): Promise<void> {
-		return await this.storage.clear();
-	}
+  async clear(): Promise<void> {
+    return await this.storage.clear();
+  }
 
-	async logStorage() {
-		console.log(`This NodeStorage has ${await this.storage.length()} keys: ${JSON.stringify(await this.storage.keys())}`);
-	}
+  async logStorage() {
+    console.log(`This NodeStorage has ${await this.storage.length()} keys: ${JSON.stringify(await this.storage.keys())}`);
+  }
 }
